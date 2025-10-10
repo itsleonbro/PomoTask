63af3f0555293b066cc2aaa5c406200a13423b97
Boipelo Ngakane <boipelongakane16@gmail.com>
2025-10-10 13:46:18 +0200

    change the css referencing error

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import styles from "./PomodoroCard.module.css";
+import styles from "./Timer.module.css";
 
 const INITIAL_TIME = 25 * 60; // 25 minutes
 
