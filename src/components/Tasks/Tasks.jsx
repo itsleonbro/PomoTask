b541076acad091effb885903a0cec561d9fd0870
MmeliGaba <mmeligabrieldyantyi@gmail.com>
2025-10-10 15:23:06 +0200

    CRUD function - Edit task

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -9,6 +9,8 @@ const Tasks = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
   const [showForm, setShowForm] = useState(false);
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [editedTitle, setEditedTitle] = useState("");
 
   const handleAddClick = () => {
     setShowForm(true);
@@ -17,6 +19,20 @@ const Tasks = () => {
   const handleDelete = (id) => {
     const updatedTask = tasks.filter((task) => task.id !== id);
     setTasks(updatedTask);
+  };
+
+  const handleEdit = (id, currentTitle) => {
+    setEditTaskId(id);
+    setEditedTitle(currentTitle);
+  };
+
+  const handleSaveEdit = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, title: editedTitle } : task
+    );
+    setTasks(updatedTasks);
+    setEditTaskId(null);
+    setEditedTitle("");
   };
 
   const handleSubmit = (e) => {
@@ -75,7 +91,18 @@ const Tasks = () => {
           {tasks.map((task) => (
             <div key={task.id} className={styles.task}>
               <div className={styles.leftSide}>
-                <h3>{task.title}</h3>
+                {editTaskId === task.id ? (
+                  <input
+                    type="text"
+                    value={editedTitle}
+                    onChange={(e) => setEditedTitle(e.target.value)}
+                    onBlur={() => handleSaveEdit(task.id)}
+                    autoFocus
+                  />
+                ) : (
+                  <h3>{task.title}</h3>
+                )}
+
                 <div className={styles.taskInfo}>
                   <div className={styles.taskStatus}>{task.status}</div>
                   <div className={styles.taskSessions}>
@@ -89,9 +116,10 @@ const Tasks = () => {
                   <span>
                     <FaCheckCircle color="#65a30d" size={25} />
                   </span>
-                  <span>
+                  <span onClick={() => handleEdit(task.id, task.title)}>
                     <FaPen color="#2563eb" size={25} />
                   </span>
+
                   <span onClick={() => handleDelete(task.id)}>
                     <FaRegTrashAlt color="#dc2626" size={25} />
                   </span>
