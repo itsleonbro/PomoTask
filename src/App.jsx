a9076c9af5baa689e06c661725039494836aa3bc
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-10-20 12:46:21 +0200

    add vercel analytics dependency and integrate it into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import Home from "./pages/Home";
 
+import { Analytics } from "@vercel/analytics/react";
+
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <Home />
+      <Analytics />
     </>
   );
 };
